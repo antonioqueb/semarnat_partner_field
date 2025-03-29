f5ade8cacf8eb039a469fe35da940965fd116764
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-29 06:09:22 -0600

    Up

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,3 +4,7 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     semarnat_permit = fields.Char(string="Permiso SEMARNAT", help="Número de permiso ambiental para operar con residuos peligrosos.")
+    semarnat_expiration = fields.Date(
+    string="Fecha de expiración del permiso",
+    help="Fecha en que expira el permiso SEMARNAT. Este campo es opcional."
+)
